882adcdfdd886e6302587f05d8f335405f9dcf1c
unknown <qazmlpok@gmail.com>
2022-02-12 14:23:31 -0500

    Fix png images not showing up in the filelistpane

--- quivilib/gui/art.py
+++ quivilib/gui/art.py
@@ -5,9 +5,9 @@ from quivilib.resources import images
 import wx
 import sys
 
-ART_THUMBNAIL_VIEW = 'quiviART_THUMBNAIL_VIEW'
+ART_THUMBNAIL_VIEW = b'quiviART_THUMBNAIL_VIEW'
 
-_IMG_MAP_16 = {
+raw_IMG_MAP_16 = {
     wx.ART_FOLDER_OPEN: 'folder_yellow_open',
     wx.ART_GO_DIR_UP: 'folder_green_up',
     wx.ART_ADD_BOOKMARK: 'folder_yellow_add_favorite',
@@ -15,14 +15,16 @@ _IMG_MAP_16 = {
     ART_THUMBNAIL_VIEW: 'view_icon',
 }
 
-
-
 class QuiviArtProvider(wx.ArtProvider):
+    def __init__(self, *args, **kwds):
+        #the wx.* constants are binary strings, while the incoming artids are unicode strings.
+        #Convert the lookup table to use unicode strings, and reference that.
+        self._IMG_MAP_16 = {k.decode('ascii'): v for k, v in raw_IMG_MAP_16.items()}
+        wx.ArtProvider.__init__(self, *args, **kwds)
     def CreateBitmap(self, artid, client, size):
         bmp = wx.NullBitmap
         if size.width == 16:
-            if artid in _IMG_MAP_16:
-                return getattr(images, _IMG_MAP_16[artid]).Bitmap
+            if artid in self._IMG_MAP_16:
+                return getattr(images, self._IMG_MAP_16[artid]).Bitmap
         
         return bmp
-    
