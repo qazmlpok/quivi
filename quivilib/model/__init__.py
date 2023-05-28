9addaf0c95c4b80a15ed208a0a20a7a68c5c80f3
unknown <qazmlpok@gmail.com>
2023-05-28 19:10:19 -0400

    Revert "Prevent loading the startup dir if there's a path from the command line"
    This reverts commit c5c5cb01c193db095901305430db585dfdbd8ba3.

--- quivilib/model/__init__.py
+++ quivilib/model/__init__.py
@@ -1,6 +1,10 @@
+
+
 from quivilib.model.container.directory import DirectoryContainer
 from quivilib.model.canvas import Canvas
 from quivilib.model.favorites import Favorites
+
+
 
 class App(object):
     def __init__(self, settings, start_dir):
