95278797af0feaacfe2f2a9b48c3d20ce0ab704f
qazmlpok <qazmlpok@gmail.com>
2026-08-29 14:05:50 -0400

    Split mouse options into a separate file

---
+++ quivilib/gui/options/mouse_options_panel.py
@@ -0,0 +1,132 @@
+import wx
+
+from quivilib.i18n import _
+from quivilib.model import Settings
+from quivilib.model.command import Command
+from quivilib.model.commandenum import CommandFlags
+from quivilib.model.options import Options
+
+
+class MouseOptionsPanel(wx.Panel):
+    def __init__(self, parent: wx.Window, settings: Settings, commands: list[Command]):
+        # begin wxGlade: OptionsDialog.__init__
+        super().__init__(parent=parent, id=-1)
+
+        self._init_mouse()
+
+        self.set_mouse_cbo(commands)
+        self.__set_properties(settings)
+        self.__do_layout()
+
+        # end wxGlade
+
+    def _init_mouse(self):
+        def _make_mouse_cbo(text):
+            lbl = wx.StaticText(self, -1, text)
+            cbo = wx.ComboBox(self, -1, choices=[], style=wx.CB_DROPDOWN|wx.CB_READONLY)
+            return (lbl, cbo)
+        (self.mouse_left_lbl,self.mouse_left_cbo) = _make_mouse_cbo(_("Left click"))
+        (self.mouse_middle_lbl,self.mouse_middle_cbo) = _make_mouse_cbo(_("Middle click"))
+        (self.mouse_right_lbl,self.mouse_right_cbo) = _make_mouse_cbo(_("Right click"))
+        (self.mouse_aux1_lbl,self.mouse_aux1_cbo) = _make_mouse_cbo(_("Aux1 click"))
+        (self.mouse_aux2_lbl,self.mouse_aux2_cbo) = _make_mouse_cbo(_("Aux2 click"))
+        self._mouse_cbos = (self.mouse_left_cbo, self.mouse_middle_cbo, self.mouse_right_cbo, self.mouse_aux1_cbo, self.mouse_aux2_cbo)
+        
+        #This looks worse than I hoped, but I think it's still better than nothing.
+        self.mouse_separator = wx.StaticLine(self, size=wx.Size(100, 1), style=wx.LI_HORIZONTAL)
+        
+        self.always_drag_chk = wx.CheckBox(self, -1, _("Always drag image with left mouse"))
+        self.threshold_lbl = wx.StaticText(self, -1, _("Threshold:"))    #TODO: Better text.
+        self.pixels_lbl = wx.StaticText(self, -1, _("px"))
+        self.threshold_txt = wx.TextCtrl(self, -1)
+        #This doesn't return the size of the "padding" - and it changes on different platforms/themes.
+        sz = self.threshold_txt.GetTextExtent('99')
+        self.threshold_txt.SetInitialSize(wx.Size(sz.x+30, -1))
+
+        self.hide_cursor_lbl = wx.StaticText(self, -1, _("Hide mouse cursor if not moved for"))
+        self.hide_cursor_post_lbl = wx.StaticText(self, -1, _("seconds"))
+        self.hide_cursor_txt = wx.TextCtrl(self, -1)
+        self.hide_cursor_txt.SetInitialSize(wx.Size(sz.x + 30, -1))
+        
+    def set_mouse_cbo(self, commands: list[Command]):
+        for m in self._mouse_cbos:
+            m.Append(_("None"), -1)
+        #Commands will be in the same order they are defined in MenuDefinitionList. Same-groups are inherently together.
+        for cmd in commands:
+            text = cmd.name_and_category
+            if cmd.flags & CommandFlags.MOUSE:
+                for m in self._mouse_cbos:
+                    m.Append(text, cmd.ide)
+
+    def __set_properties(self, settings: Settings):
+        self._set_selected(self.mouse_left_cbo, settings.getint('Mouse', 'LeftClickCmd'))
+        self._set_selected(self.mouse_middle_cbo, settings.getint('Mouse', 'MiddleClickCmd'))
+        self._set_selected(self.mouse_right_cbo, settings.getint('Mouse', 'RightClickCmd'))
+        self._set_selected(self.mouse_aux1_cbo, settings.getint('Mouse', 'Aux1ClickCmd'))
+        self._set_selected(self.mouse_aux2_cbo, settings.getint('Mouse', 'Aux2ClickCmd'))
+        always_drag = (settings.get('Mouse', 'AlwaysLeftMouseDrag') == '1')
+        self.always_drag_chk.SetValue(always_drag)
+        self.threshold_txt.SetValue(settings.get('Mouse', 'DragThreshold'))
+        self.hide_cursor_txt.SetValue(settings.get('Mouse', 'HideMouseDuration'))
+
+    def __do_layout(self):
+        # begin wxGlade: OptionsDialog.__do_layout
+        mouse_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # Mouse bindings
+        mouse_sizer.Add(self.mouse_left_lbl, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_left_cbo, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_middle_lbl, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_middle_cbo, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_right_lbl, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_right_cbo, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_aux1_lbl, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_aux1_cbo, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_aux2_lbl, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_aux2_cbo, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(self.mouse_separator, 0, wx.TOP | wx.EXPAND, 10)
+
+        # Drag checkbox and treshold
+        mouse_drag_sizer = wx.BoxSizer(wx.VERTICAL)
+        mouse_drag_sizer_nested = wx.BoxSizer(wx.HORIZONTAL)
+        mouse_drag_sizer.Add(self.always_drag_chk, 0, wx.LEFT | wx.TOP, 5)
+        mouse_drag_sizer_nested.Add(self.threshold_lbl, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
+        mouse_drag_sizer_nested.Add(self.threshold_txt, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
+        mouse_drag_sizer_nested.Add(self.pixels_lbl, 0, wx.ALIGN_CENTER_VERTICAL, 5)
+        mouse_drag_sizer.Add(mouse_drag_sizer_nested, 0, wx.ALIGN_RIGHT | wx.LEFT | wx.RIGHT | wx.TOP, 5)
+        mouse_sizer.Add(mouse_drag_sizer, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+
+        # Hide mouse
+        mouse_hide_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        mouse_hide_sizer.Add(self.hide_cursor_lbl, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
+        mouse_hide_sizer.Add(self.hide_cursor_txt, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
+        mouse_hide_sizer.Add(self.hide_cursor_post_lbl, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
+        mouse_sizer.Add(mouse_hide_sizer, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
+
+        self.SetSizer(mouse_sizer)
+        self.Layout()
+        # end wxGlade
+
+    def on_ok(self, opt: Options): # wxGlade: OptionsDialog.<event_handler>
+        #TODO: Use a separate object. Options is also too big.
+        sel = self.mouse_left_cbo.GetSelection()
+        opt.left_click_cmd = self.mouse_left_cbo.GetClientData(sel)
+        sel = self.mouse_middle_cbo.GetSelection()
+        opt.middle_click_cmd = self.mouse_left_cbo.GetClientData(sel)
+        sel = self.mouse_right_cbo.GetSelection()
+        opt.right_click_cmd = self.mouse_left_cbo.GetClientData(sel)
+        sel = self.mouse_aux1_cbo.GetSelection()
+        opt.aux1_click_cmd = self.mouse_left_cbo.GetClientData(sel)
+        sel = self.mouse_aux2_cbo.GetSelection()
+        opt.aux2_click_cmd = self.mouse_left_cbo.GetClientData(sel)
+
+        opt.always_drag = self.always_drag_chk.GetValue()
+        opt.drag_threshold = self.threshold_txt.GetValue()
+        opt.hide_mouse_duration = self.hide_cursor_txt.GetValue()
+
+    @staticmethod
+    def _set_selected(control: wx.ComboBox, item):
+        for i in range(control.GetCount()):
+            if control.GetClientData(i) == item:
+                control.SetSelection(i)
+                break
--- quivilib/gui/options/options.py
+++ quivilib/gui/options/options.py
@@ -7,7 +7,9 @@ from pubsub import pub as Publisher
 from wx.lib import langlistctrl
 
 import quivilib.gui.hotkeyctrl as hk
+from quivilib.gui.options.mouse_options_panel import MouseOptionsPanel
 from quivilib.i18n import _
+from quivilib.model import Settings
 from quivilib.model.command import Command
 from quivilib.model.commandenum import CommandFlags, FitSettings
 from quivilib.model.options import Options
@@ -16,7 +18,7 @@ from quivilib.model.shortcut import Shortcut
 WINDOW_SIZE = (500, 780)
 
 class OptionsDialog(wx.Dialog):
-    def __init__(self, parent, fit_choices: list[tuple[str, FitSettings.FitType]], settings, commands: list[Command],
+    def __init__(self, parent, fit_choices: list[tuple[str, FitSettings.FitType]], settings: Settings, commands: list[Command],
                  available_languages: list[wx.Language], active_language: wx.Language, save_locally: bool):
         self.fit_choices = fit_choices
         self.save_locally = save_locally
@@ -25,14 +27,13 @@ class OptionsDialog(wx.Dialog):
         # begin wxGlade: OptionsDialog.__init__
         wx.Dialog.__init__(self, parent=parent, style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER)
         self.main_notebook = wx.Notebook(self, -1, style=wx.NB_TOP)
-        self.mouse_pane = wx.Panel(self.main_notebook, -1)
+        self.mouse_pane = MouseOptionsPanel(self.main_notebook, settings, commands)
         self.language_pane = wx.Panel(self.main_notebook, -1)
         self.keys_pane = wx.Panel(self.main_notebook, -1)
         self.viewing_pane = wx.Panel(self.main_notebook, -1)
         
         self._init_viewing()
         self._init_commands()
-        self._init_mouse()
         
         self.lang_lst = langlistctrl.LanguageListCtrl(self.language_pane, -1, style=wx.LC_REPORT|wx.LC_NO_HEADER, filter=langlistctrl.LC_ONLY, only=available_languages, select=active_language)
 
@@ -99,60 +100,18 @@ class OptionsDialog(wx.Dialog):
         self.shortcut_assign_btn = wx.Button(self.keys_pane, -1, _("Assign"))
         self.assigned_command_lbl = wx.StaticText(self.keys_pane, -1, "")
         self.reset_btn = wx.Button(self.keys_pane, -1, _("Reset all to defaults"))
-    def _init_mouse(self):
-        def _make_mouse_cbo(text):
-            lbl = wx.StaticText(self.mouse_pane, -1, text)
-            cbo = wx.ComboBox(self.mouse_pane, -1, choices=[], style=wx.CB_DROPDOWN|wx.CB_READONLY)
-            return (lbl, cbo)
-        (self.mouse_left_lbl,self.mouse_left_cbo) = _make_mouse_cbo(_("Left click"))
-        (self.mouse_middle_lbl,self.mouse_middle_cbo) = _make_mouse_cbo(_("Middle click"))
-        (self.mouse_right_lbl,self.mouse_right_cbo) = _make_mouse_cbo(_("Right click"))
-        (self.mouse_aux1_lbl,self.mouse_aux1_cbo) = _make_mouse_cbo(_("Aux1 click"))
-        (self.mouse_aux2_lbl,self.mouse_aux2_cbo) = _make_mouse_cbo(_("Aux2 click"))
-        self._mouse_cbos = (self.mouse_left_cbo, self.mouse_middle_cbo, self.mouse_right_cbo, self.mouse_aux1_cbo, self.mouse_aux2_cbo)
-        
-        #This looks worse than I hoped, but I think it's still better than nothing.
-        self.mouse_separator = wx.StaticLine(self.mouse_pane, size=wx.Size(100, 1), style=wx.LI_HORIZONTAL)
-        
-        self.always_drag_chk = wx.CheckBox(self.mouse_pane, -1, _("Always drag image with left mouse"))
-        self.threshold_lbl = wx.StaticText(self.mouse_pane, -1, _("Threshold:"))    #TODO: Better text.
-        self.pixels_lbl = wx.StaticText(self.mouse_pane, -1, _("px"))
-        self.threshold_txt = wx.TextCtrl(self.mouse_pane, -1)
-        #This doesn't return the size of the "padding" - and it changes on different platforms/themes.
-        sz = self.threshold_txt.GetTextExtent('99')
-        self.threshold_txt.SetInitialSize(wx.Size(sz.x+30, -1))
 
-        self.hide_cursor_lbl = wx.StaticText(self.mouse_pane, -1, _("Hide mouse cursor if not moved for"))
-        self.hide_cursor_post_lbl = wx.StaticText(self.mouse_pane, -1, _("seconds"))
-        self.hide_cursor_txt = wx.TextCtrl(self.mouse_pane, -1)
-        self.hide_cursor_txt.SetInitialSize(wx.Size(sz.x + 30, -1))
-        
     def __set_properties(self):
         # begin wxGlade: OptionsDialog.__set_properties
         self.SetTitle(_("Options"))
         self.fit_cbo.SetSelection(-1)
         # end wxGlade
-        
-        for m in self._mouse_cbos:
-            m.Append(_("None"), -1)
+
         #Commands will be in the same order they are defined in MenuDefinitionList. Same-groups are inherently together.
         for cmd in self.commands:
             text = cmd.name_and_category
             if cmd.flags & CommandFlags.KB:
                 self.commands_lst.Append(text, cmd)
-            if cmd.flags & CommandFlags.MOUSE:
-                for m in self._mouse_cbos:
-                    m.Append(text, cmd.ide)
-
-        self._set_selected(self.mouse_left_cbo, self.settings.getint('Mouse', 'LeftClickCmd'))
-        self._set_selected(self.mouse_middle_cbo, self.settings.getint('Mouse', 'MiddleClickCmd'))
-        self._set_selected(self.mouse_right_cbo, self.settings.getint('Mouse', 'RightClickCmd'))
-        self._set_selected(self.mouse_aux1_cbo, self.settings.getint('Mouse', 'Aux1ClickCmd'))
-        self._set_selected(self.mouse_aux2_cbo, self.settings.getint('Mouse', 'Aux2ClickCmd'))
-        always_drag = (self.settings.get('Mouse', 'AlwaysLeftMouseDrag') == '1')
-        self.always_drag_chk.SetValue(always_drag)
-        self.threshold_txt.SetValue(self.settings.get('Mouse', 'DragThreshold'))
-        self.hide_cursor_txt.SetValue(self.settings.get('Mouse', 'HideMouseDuration'))
 
         setting_fit_type = FitSettings.get_fittype(self.settings.get('Options', 'FitType'))
         for name, fit_type in self.fit_choices:
@@ -206,7 +165,6 @@ class OptionsDialog(wx.Dialog):
         
         self.__do_layout_viewing()
         self.__do_layout_keys()
-        self.__do_layout_mouse()
         #Language
         language_sizer = wx.BoxSizer(wx.VERTICAL)
         language_sizer.Add(self.lang_lst, 1, wx.ALL|wx.EXPAND, 5)
@@ -224,40 +182,6 @@ class OptionsDialog(wx.Dialog):
         self.SetSizer(main_sizer)
         self.Layout()
         # end wxGlade
-    def __do_layout_mouse(self):
-        mouse_sizer = wx.BoxSizer(wx.VERTICAL)
-
-        # Mouse bindings
-        mouse_sizer.Add(self.mouse_left_lbl, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_left_cbo, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_middle_lbl, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_middle_cbo, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_right_lbl, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_right_cbo, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_aux1_lbl, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_aux1_cbo, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_aux2_lbl, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_aux2_cbo, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(self.mouse_separator, 0, wx.TOP|wx.EXPAND, 10)
-
-        # Drag checkbox and treshold
-        mouse_drag_sizer = wx.BoxSizer(wx.VERTICAL)
-        mouse_drag_sizer_nested = wx.BoxSizer(wx.HORIZONTAL)
-        mouse_drag_sizer.Add(self.always_drag_chk, 0, wx.LEFT|wx.TOP, 5)
-        mouse_drag_sizer_nested.Add(self.threshold_lbl, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
-        mouse_drag_sizer_nested.Add(self.threshold_txt, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
-        mouse_drag_sizer_nested.Add(self.pixels_lbl, 0, wx.ALIGN_CENTER_VERTICAL, 5)
-        mouse_drag_sizer.Add(mouse_drag_sizer_nested, 0, wx.ALIGN_RIGHT|wx.LEFT|wx.RIGHT|wx.TOP, 5)
-        mouse_sizer.Add(mouse_drag_sizer, 0, wx.LEFT|wx.RIGHT|wx.TOP, 5)
-
-        # Hide mouse
-        mouse_hide_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        mouse_hide_sizer.Add(self.hide_cursor_lbl, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
-        mouse_hide_sizer.Add(self.hide_cursor_txt, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
-        mouse_hide_sizer.Add(self.hide_cursor_post_lbl, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
-        mouse_sizer.Add(mouse_hide_sizer, 0, wx.LEFT | wx.RIGHT | wx.TOP, 5)
-        
-        self.mouse_pane.SetSizer(mouse_sizer)
     def __do_layout_keys(self):
         keys_sizer = wx.BoxSizer(wx.VERTICAL)
         shortcuts_sizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -367,6 +291,7 @@ class OptionsDialog(wx.Dialog):
 
     def on_ok(self, event: wx.CommandEvent): # wxGlade: OptionsDialog.<event_handler>
         opt = Options()
+        self.mouse_pane.on_ok(opt)
         sel = self.fit_cbo.GetSelection()
         opt.fit_type = self.fit_cbo.GetClientData(sel)
         opt.fit_width_str = self.width_txt.GetValue()
@@ -374,18 +299,7 @@ class OptionsDialog(wx.Dialog):
         opt.custom_bg = self.bg_color_custom_rdo.GetValue()
         opt.custom_bg_color = self.bg_color_picker.GetColour()
         opt.language = self.lang_lst.GetLanguage()
-        
-        sel = self.mouse_left_cbo.GetSelection()
-        opt.left_click_cmd = self.mouse_left_cbo.GetClientData(sel)
-        sel = self.mouse_middle_cbo.GetSelection()
-        opt.middle_click_cmd = self.mouse_left_cbo.GetClientData(sel)
-        sel = self.mouse_right_cbo.GetSelection()
-        opt.right_click_cmd = self.mouse_left_cbo.GetClientData(sel)
-        sel = self.mouse_aux1_cbo.GetSelection()
-        opt.aux1_click_cmd = self.mouse_left_cbo.GetClientData(sel)
-        sel = self.mouse_aux2_cbo.GetSelection()
-        opt.aux2_click_cmd = self.mouse_left_cbo.GetClientData(sel)
-        
+
         opt.save_locally = self.settings_local_chk.GetValue()
         opt.real_fullscreen = self.real_fullscreen_chk.GetValue()
         opt.open_first = self.open_first_chk.GetValue()
@@ -397,9 +311,6 @@ class OptionsDialog(wx.Dialog):
         opt.placeholder_autoopen = self.placeholder_autoopen_chk.GetValue()
         opt.placeholder_separate = self.placeholder_separate_chk.GetValue()
         opt.shortcuts = self.shortcuts
-        opt.always_drag = self.always_drag_chk.GetValue()
-        opt.drag_threshold = self.threshold_txt.GetValue()
-        opt.hide_mouse_duration = self.hide_cursor_txt.GetValue()
         opt.darkmode = self.dark_mode_system_rdo.GetSelection()
         
         #TODO: (2,2) Improve: handle errors here
